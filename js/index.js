45be1501544c707e224ad4846c31724e9fb123ba
David <iwasokunoluwafemi@gmail.com>
2024-09-05 15:09:34 +0000

    update

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
 let menu = document.querySelector('#menu-icon')
-let navlink = document.querySelector('.navlink')
+let navlink = document.querySelector('#head1')
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x')
-    navlink.classList.toggle('open')
+    if(menu.classList.contains("bx-bar")){
+      menu.classList.add("bx-barshow")
+      menu.classList.remove("bx-bar")
+      navlink.classList.add('navlink')
+        navlink.classList.remove('close')
+    }else{
+        menu.classList.add("bx-bar")
+        menu.classList.remove("bx-barshow")
+        navlink.classList.remove('navlink')
+        navlink.classList.add('close')
+    }
+
+    // navlink.classList.toggle('open')
 }
