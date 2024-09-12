abd8f3fd581e91678b6418b7b2612cc7a54c3f86
David <iwasokunoluwafemi@gmail.com>
2024-09-12 15:17:54 +0000

    solar

--- js/index.js
+++ js/index.js
@@ -16,3 +16,24 @@ menu.onclick = () => {
 
     // navlink.classList.toggle('open')
 }
+
+
+
+// let menu = document.querySelector('#menu-icon')
+// let navlink = document.querySelector('#head1')
+
+// menu.onclick = () => {
+//     if(menu.classList.contains("bx-bar")){
+//       menu.classList.add("bx-barshow")
+//       menu.classList.remove("bx-bar")
+//       navlink.classList.add('navlink')
+//         navlink.classList.remove('close')
+//     }else{
+//         menu.classList.add("bx-bar")
+//         menu.classList.remove("bx-barshow")
+//         navlink.classList.remove('navlink')
+//         navlink.classList.add('close')
+//     }
+
+//     // navlink.classList.toggle('open')
+// }
